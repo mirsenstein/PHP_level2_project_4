56b2a63422c4e730d3aac865e19695326887f474
Mirsi <mirela.katrankieva@inveitix.com>
2019-03-21 17:54:29 +0200

    updated DB

--- basic_fb.sql
+++ basic_fb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 07, 2019 at 05:52 PM
+-- Generation Time: Mar 21, 2019 at 04:53 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -35,6 +35,34 @@ CREATE TABLE `comments` (
   `comment` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `post_id`, `user_id`, `comment`) VALUES
+(5, 25, 10, 'Oh, yeah!'),
+(6, 31, 10, 'Ð˜ Ð°Ð· Ð¸ÑÐºÐ°Ð¼ Ð´Ð° ÐºÐ¾Ð¼ÐµÐ½Ñ‚Ð¸Ñ€Ð°Ð¼!');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `like_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`like_id`, `post_id`, `user_id`) VALUES
+(16, 24, 10),
+(15, 25, 9);
+
 -- --------------------------------------------------------
 
 --
@@ -44,11 +72,27 @@ CREATE TABLE `comments` (
 CREATE TABLE `posts` (
   `post_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `text` text COLLATE utf8_unicode_ci NOT NULL,
+  `post_text` text COLLATE utf8_unicode_ci,
   `image` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
   `date_time` datetime NOT NULL,
   `likes_num` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`post_id`, `user_id`, `post_text`, `image`, `date_time`, `likes_num`) VALUES
+(24, 9, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', NULL, '2019-03-20 20:39:37', 0),
+(25, 9, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'dog.jpg', '2019-03-20 08:40:08', 0),
+(27, 10, '', NULL, '2019-03-20 20:50:16', 0),
+(28, 10, '', 'Tina-copertina.jpg', '2019-03-20 20:51:27', 0),
+(29, 10, 'Run, wolf! RUN!', NULL, '2019-03-20 20:59:06', 0),
+(30, 10, '', NULL, '2019-03-20 21:00:12', 0),
+(31, 10, '', NULL, '2019-03-20 21:01:02', 0),
+(32, 10, '', NULL, '2019-03-20 21:06:05', 0),
+(33, 9, '', NULL, '2019-03-20 21:10:50', 0),
+(34, 9, '', NULL, '2019-03-20 21:11:46', 0);
 
 -- --------------------------------------------------------
 
@@ -64,6 +108,14 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `names`) VALUES
+(9, 'user1', '$2y$10$jIvHs/j/tpSKUarEqbtUoOjZwbRwvUTWUiTlksTMw6SPbGQoKzfbm', 'The most creative'),
+(10, '2user', '$2y$10$jKfxClrJ63Rl8.Alr3PwXeNr4KacAd1FGDdGCCtHtBXdM8IHV2htS', 'YEAH!!!!');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -72,6 +124,13 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `post_id` (`post_id`,`user_id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`like_id`),
   ADD KEY `post_id` (`post_id`,`user_id`);
 
 --
@@ -95,19 +154,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
